af955c5ac41026608d0447e35a98e68aee861060
Uwe Meier <uwe.meier@sclable.com>
2019-10-08 09:20:51 +0200

    chore: Xcode setup

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '11.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'RealmCrashExample' do
+
+    pod 'Realm', '~> 3.19'
+
+end
---
+++ RealmCrashExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmCrashExample
+//
+//  Created by Uwe Meier on 08.10.19.
+//  Copyright © 2019 Uwe Meier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
